de79b3a9cccdb25d3ee2a22fd2646ab71443c695
liying2008 <liruoer2008@yeah.net>
2022-12-20 22:48:40 +0800

    chore: add eslint and stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
 import manifest from './manifest.json' assert { type: 'json' }
 
+export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': `${r('src')}/`,
+    },
+  },
   plugins: [
     vue(),
     crx({ manifest }),
