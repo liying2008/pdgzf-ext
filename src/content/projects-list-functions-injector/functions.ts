e17cd953b5083a158a51dc1481e19ad703d69385
liying2008 <liruoer2008@yeah.net>
2023-01-02 22:20:06 +0800

    feat: add project property to page

--- src/content/projects-list-functions-injector/functions.ts
+++ src/content/projects-list-functions-injector/functions.ts
@@ -6,6 +6,7 @@ export function enable(projectProperties: ProjectProperty[], ppv: ProjectPropert
     return
   }
   console.log(listElem)
+  renderPPV(listElem as HTMLElement, projectProperties, ppv)
 }
 
 function isProjectsListPage() {
@@ -13,4 +14,46 @@ function isProjectsListPage() {
   const element = document.querySelector(selector)
   console.log('isProjectsListPage', element)
   return element
+}
+
+function renderPPV(listElem: HTMLElement, projectProperties: ProjectProperty[], ppv: ProjectPropertyValues) {
+  const lis = listElem.querySelectorAll('li')
+  lis.forEach((elem) => {
+    const infoBlock = elem.querySelector('div.marL30')
+    const title = infoBlock?.querySelector('h4')?.textContent || ''
+    infoBlock?.append(...generatePPVElems(title, projectProperties, ppv))
+  })
+}
+
+function generatePPVElems(project: string, projectProperties: ProjectProperty[], ppv: ProjectPropertyValues) {
+  const elems: Node[] = []
+  const len = projectProperties.length
+  for (let i = 0; i < len; i++) {
+    const pp = projectProperties[i]
+
+    const nameElem = document.createElement('span')
+    nameElem.classList.add('bold')
+    nameElem.textContent = `${pp.name}：`
+
+    const valueElem = document.createElement('span')
+    valueElem.classList.add('c-7')
+    valueElem.textContent = getValue(project, pp.id, ppv) || '<未知>'
+
+    const pElem = document.createElement('p')
+    pElem.classList.add('c-6', 'marT10', 'extra-p')
+    pElem.append(nameElem, valueElem)
+
+    elems.push(pElem)
+  }
+
+  return elems
+}
+
+
+function getValue(project: string, property: string, ppv: ProjectPropertyValues) {
+  if (project in ppv && property in ppv[project]) {
+    return ppv[project][property]
+  } else {
+    return undefined
+  }
 }
